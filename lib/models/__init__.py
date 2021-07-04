2e554fcab10e540f4cb9e80f73337b4a07480f3b
leeyegy <35657511+leeyegy@users.noreply.github.com>
2021-07-04 22:15:38 +0800

    Add files via upload

---
+++ lib/models/__init__.py
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# Modified by Hanbin Dai (daihanbin.ac@gmail.com) and Feng Zhang (zhangfengwcy@gmail.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.pose_tokenpose_t
+import models.pose_tokenpose_s
+import models.pose_tokenpose_b
+import models.pose_tokenpose_l
+
+
+
+
+
+
+
+
+
+
+
+
